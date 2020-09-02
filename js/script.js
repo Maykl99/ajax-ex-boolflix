cccd25619cda48c32709039c5ac6d6edd29f7194
Maykl Hakobyan <maicolhakobyan853@yahoo.it>
2020-09-02 15:49:57 +0200

    app js

--- js/script.js
+++ js/script.js
@@ -11,47 +11,75 @@ Voto */
 
 $(document).ready(function(){
 
-    var nome='';
+    nome='';
    
     $('button').click(function(){
         nome= $('#prelevaNome').val();
-
         if(nome !=''){
-            $.ajax({
-                type: "GET",
-                url: "https://api.themoviedb.org/3/search/movie",
-                data: {
-                    api_key: '2c42a4436c6db0bbb4e23ee64ca1bc93',
-                    query: nome,
-                    language: 'it-IT',
-                },
-                success: function (response) {
-                    for(var i=0; i<response.results.length; i++){
-                        var source = $("#entry-template").html();
-                        var template = Handlebars.compile(source);
-                        var context = { title: response.results[i].title, original_language: response.results[i].original_language, original_title: response.results[i].original_title,vote_average: response.results[i].vote_average};
-                        var html = template(context);
-                        $('.container').append(html);
-                        $('#prelevaNome').val('');
-                        $('button#cancella').click(function(){
-                            $('.container').text('');
-                            //.empty()
-                            
-                        });
-                    }
-                },
-                error: function(){
-                    alert('errore')
-                } 
-            });
+            chiamataAjax();
         }
         else if(nome == ''){
-            
             $('.container').html('<h1>Inserisci un valore</h1>'); 
         }
     
     })
 
+    $("#prelevaNome").keydown(function(event){
+        if(event.which == 13 || event.keyCode == 13){
+            nome= $('#prelevaNome').val();
+            if(nome !== ""){
+                chiamataAjax();
+                $('#prelevaNome').val('');
+            }else if(nome == ''){
+                $('.container').html('<h1>Inserisci un valore</h1>'); 
+            }
+        }
+    })
+
   
 
 });
+
+
+function chiamataAjax(){
+    $.ajax({
+        type: "GET",
+        url: "https://api.themoviedb.org/3/search/movie",
+        data: {
+            api_key: '2c42a4436c6db0bbb4e23ee64ca1bc93',
+            query: nome,
+            language: 'it-IT',
+        },
+        success: function (response) {
+                chiamataApi(response);
+        
+        },
+        error: function(){
+            alert('errore')
+        } 
+    });
+}
+
+function chiamataApi(response){
+    for(var i=0; i<response.results.length; i++){
+
+        var source = $("#entry-template").html();
+        var template = Handlebars.compile(source);
+
+        var context = { 
+            title: response.results[i].title, 
+            original_language: response.results[i].original_language, 
+            riginal_title: response.results[i].original_title,
+            vote_average: response.results[i].vote_average
+        };
+
+        var html = template(context);
+        $('.container').append(html);
+        $('#prelevaNome').val('');
+        $('button#cancella').click(function(){
+            $('.container').text('');
+            //.empty()
+            
+        });
+    }
+}
